939400026cfc5a3c45b4a284889281e8ab3f4e1f
destro7er987 <destro7er987@gmail.com>
2020-09-28 16:31:29 +0800

    依不同狀態送出 alert, normal 訊息給控制器

--- SmartHome/SmartHome.ino
+++ SmartHome/SmartHome.ino
@@ -3,24 +3,42 @@ void setup()
 pinMode(13, OUTPUT);
 pinMode(12, OUTPUT);
 pinMode(11, OUTPUT);
+pinMode(2, INPUT);
+Serial.begin(9600);
 }
 void loop()
 {
+  if(digitalRead(2)==HIGH){
+    Serial.println("alert");
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    tone(8,1000,500);
+    delay(250);
+  }
+  else {
+    Serial.println("normal");
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    tone(8,1000,500);
+    delay(250);
+  }
 digitalWrite(13, HIGH);
 digitalWrite(12, LOW);
 digitalWrite(11, LOW);
-tone(8,2000,1000);
+tone(8,1000,500);
 delay(250);
 
 digitalWrite(13, LOW);
 digitalWrite(12, HIGH);
 digitalWrite(11, LOW);
-tone(8,2000,1000);
+tone(8,750,500);
 delay(250);
 
 digitalWrite(13, LOW);
 digitalWrite(12, LOW);
 digitalWrite(11, HIGH);
-tone(8,2000,1000);
+tone(8,500,500);
 delay(250);
 }
